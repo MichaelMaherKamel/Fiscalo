d19464d99445de0cd4fa2b00efcf82879389a44d
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-04-08 23:59:40 +0200

    DB init

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import config from './config.js';
 
 /* Congigurations */
-dotenv.config();
+dotenv.config({ path: '../.env' });
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -18,3 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* Mongoose Setup */
+mongoose
+  .connect(config.mongodb_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(config.port, () => console.log(`Server Port: ${config.port}`));
+  })
+  .catch((error) => console.log(`${error} didn't connect`));
