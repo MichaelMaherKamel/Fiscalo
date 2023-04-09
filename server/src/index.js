74d9cf39d1372e94f0a24c5e35e8dd788dfe9892
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-04-09 19:29:05 +0200

    BrowserLogo + DBKpiSchema

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import config from './config.js';
+import kpiRoutes from '../routes/kpi.js';
+import KPI from '../models/kpis.js';
+import { kpis } from '../data/data.js';
 
 /* Congigurations */
 dotenv.config({ path: '../.env' });
@@ -18,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* Routes */
+app.use('/kpi', kpiRoutes);
+
 /* Mongoose Setup */
 mongoose
   .connect(config.mongodb_URI, {
@@ -26,5 +32,7 @@ mongoose
   })
   .then(async () => {
     app.listen(config.port, () => console.log(`Server Port: ${config.port}`));
+    await mongoose.connection.db.dropDatabase(); //Don't do it in a real production app, it's because we are testing things out so we want to drop our DB
+    KPI.insertMany(kpis);
   })
   .catch((error) => console.log(`${error} didn't connect`));
